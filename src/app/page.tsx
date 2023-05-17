48086faeb48df8a6aa92b469e7bddb8975d0374f
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-05-16 22:24:24 -0300

    feat: create stripes effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* Left section */}
-      <div className="relative flex flex-col items-start justify-between px28 p-16 overflow-hidden">
+      <div className="relative flex flex-col items-start justify-between px28 p-16 overflow-hidden border-r border-white/10">
+        {/* Blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 blur-full rounded-full" />
+
+        {/* Stripes */}
+        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes"></div>
 
         <div className="flex flex-1 flex-col items-start justify-between">
           <div className="flex">Crie sua conta e salve suas memórias!</div>
