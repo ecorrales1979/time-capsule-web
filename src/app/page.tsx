3ae7f385b9eb0d9c268196069a607bd4eba16382
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-05-16 22:30:32 -0300

    feat: add stars bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* Left section */}
-      <div className="relative flex flex-col items-start justify-between px28 p-16 overflow-hidden border-r border-white/10">
+      <div className="relative flex flex-col items-start justify-between px28 p-16 overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
         {/* Blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 blur-full rounded-full" />
 
@@ -18,7 +18,7 @@ export default function Home() {
         </div>
       </div>
       {/* Right Section */}
-      <div className="flex flex-col p-16">
+      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
         <div className="flex flex-1 items-center justify-center">
           <p className="text-center leading-relaxed w-[360px]">
             Você ainda não registrou nenhuma lembrança, comece a{' '}
