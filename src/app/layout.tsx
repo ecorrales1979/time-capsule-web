527288266688acdeed827730b198c1d83929ec54
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-05-16 21:52:29 -0300

    feat: basic page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable} ${baijamjuree.variable} font-sans`}>{children}</body>
+      <body className={`${roboto.variable} ${baijamjuree.variable} font-sans bg-gray-900 text-gray-100`}>
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
-  )
+    <main className="grid grid-cols-2 min-h-screen">
+      {/* Left section */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 flex-col items-start justify-between px28 py-16">
+          <div className="flex">Crie sua conta e salve suas memórias!</div>
+          <div className="flex">
+            <p className="font-bold text-[40px]">Sua cápsula do tempo</p>
+          </div>
+          <div className="flex">Feito com 💜 no NLW da Rocketseat</div>
+        </div>
+      </div>
+      {/* Right Section */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">criar agora</a>!
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
