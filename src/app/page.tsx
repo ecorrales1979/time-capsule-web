672e820c5faddd3894338fa5b3c580a16fef3ae7
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-05-16 22:07:24 -0300

    feat: create bg blur efect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* Left section */}
-      <div className="flex flex-col p-16">
-        <div className="flex flex-1 flex-col items-start justify-between px28 py-16">
+      <div className="relative flex flex-col items-start justify-between px28 p-16 overflow-hidden">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 blur-full rounded-full" />
+
+        <div className="flex flex-1 flex-col items-start justify-between">
           <div className="flex">Crie sua conta e salve suas memórias!</div>
           <div className="flex">
             <p className="font-bold text-[40px]">Sua cápsula do tempo</p>
