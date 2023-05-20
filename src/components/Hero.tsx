05f2be540cc54f3458feefc7781210ff8a1ccff3
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-05-20 11:28:00 -0300

    feat: create basic new memory page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 import logo from '@/assets/spacetime-logo.svg'
 
@@ -8,11 +9,16 @@ export default function Hero() {
       <Image src={logo} alt="Spacetime logo" />
       <div className="max-w-[420px] space-y-1">
         <h1 className="text-5xl font-bold leading-tight text-gray-50">Sua cápsula do tempo</h1>
-        <p className="text-lg leading-relaxed">Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</p>
+        <p className="text-lg leading-relaxed">
+          Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
+        </p>
       </div>
-      <a href="" className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600">
+      <Link
+        href="/memories/new"
+        className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+      >
         Cadastrar lembrança
-      </a>
+      </Link>
     </div>
   )
 }
